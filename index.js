944f93716aa0deb75c6f6cc0d2f95b4dcb2b9b55
范智阔 <fzk@choicesoft.com.cn>
2020-09-16 20:26:27 +0800

    启动路径问题

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const { fork } = require("child_process");
 
 const command = process.argv.slice(2);
 
-const cp = fork("./start.js", command);
+const cp = fork(`${__dirname}/start.js`, command);
 
 cp.on("message", (data) => {
   if (data === "restart") {
     cp.kill();
-    fork("./start.js", command);
+    fork(`${__dirname}/start.js`, command);
   }
 });
