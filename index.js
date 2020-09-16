4b62c646e88e4e71df6acfccd627f2c2ab644509
范智阔 <fzk@choicesoft.com.cn>
2020-09-16 20:17:28 +0800

    组件增加自更新功能

--- index.js
+++ index.js
@@ -1,58 +1,13 @@
 #!/usr/bin/env node
-const { exec } = require("child_process");
-const { connect } = require("socket.io-client");
+const { fork } = require("child_process");
 
-(async function cook() {
-  const outs = [];
+const command = process.argv.slice(2);
 
-  const command = process.argv.slice(2);
+const cp = fork("./start.js", command);
 
-  const cp = exec(`cd ${process.cwd()} && cook ${command}`);
-
-  cp.stdout.on("data", (data) => {
-    outs.push(data);
-    console.log(data);
-  });
-
-  cp.stdout.on("end", async () => {
-    if (outs[outs.length - 1].indexOf("发布成功") === -1) {
-      return;
-    }
-    const info = await getGitInfo();
-    const branch = await getBranch();
-    const [author, commit] = info.split("#");
-
-    const out = outs[outs.length - 2];
-    const [name, version] = out.replace("+ ", "").split(/@(?=\d)/);
-
-    const io = connect("http://office.choicesaas.cn/choicefe");
-    io.emit("update", `${author}#${name}#${version}#${branch}#${commit}`);
-
-    setTimeout(() => {
-      io.close();
-    }, 1000);
-  });
-})();
-
-async function getGitInfo() {
-  return new Promise((resolve) => {
-    const cp = exec(`git log --pretty=format:"%an#%s" -1`);
-    cp.stdout.on("data", (data) => {
-      resolve(data);
-    });
-  });
-}
-
-async function getBranch() {
-  return new Promise((resolve) => {
-    const cp = exec("git branch");
-    cp.stdout.on("data", (data) => {
-      const branch = data
-        .split("\n")
-        .filter((b) => b.startsWith("*"))
-        .join("")
-        .replace("* ", "");
-      resolve(branch);
-    });
-  });
-}
+cp.on("message", (data) => {
+  if (data === "restart") {
+    cp.kill();
+    fork("./start.js", command);
+  }
+});
---
+++ start.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+const { exec } = require("child_process");
+const { connect } = require("socket.io-client");
+
+(async function cook() {
+  const latest = await getLatestVersion();
+  const local = getLocalVersion();
+
+  console.log("Latest Version: ", latest);
+  console.log("Local Version: ", local);
+  if (latest !== local) {
+    console.log("Installing latest version...");
+    updateToLatestVersion();
+    return;
+  }
+
+  const outs = [];
+
+  const command = process.argv.slice(2);
+
+  const cp = exec(`cd ${process.cwd()} && cook ${command}`);
+
+  cp.stdout.on("data", (data) => {
+    outs.push(data);
+    console.log(data);
+  });
+
+  cp.stdout.on("end", async () => {
+    if (outs[outs.length - 1].indexOf("发布成功") === -1) {
+      return;
+    }
+    const info = await getGitInfo();
+    const branch = await getBranch();
+    const [author, commit] = info.split("#");
+
+    const out = outs[outs.length - 2];
+    const [name, version] = out.replace("+ ", "").split(/@(?=\d)/);
+
+    const io = connect("http://office.choicesaas.cn/choicefe");
+    io.emit("update", `${author}#${name}#${version}#${branch}#${commit}`);
+
+    setTimeout(() => {
+      io.close();
+    }, 1000);
+  });
+})();
+
+async function getGitInfo() {
+  return new Promise((resolve) => {
+    const cp = exec(`git log --pretty=format:"%an#%s" -1`);
+    cp.stdout.on("data", (data) => {
+      resolve(data);
+    });
+  });
+}
+
+async function getBranch() {
+  return new Promise((resolve) => {
+    const cp = exec("git branch");
+    cp.stdout.on("data", (data) => {
+      const branch = data
+        .split("\n")
+        .filter((b) => b.startsWith("*"))
+        .join("")
+        .replace("* ", "");
+      resolve(branch);
+    });
+  });
+}
+
+async function getLatestVersion() {
+  return new Promise((resolve) => {
+    const cp = exec("npm view gcook version");
+    cp.stdout.on("data", (version) => {
+      resolve(version.replace(/(\r|\n)/g, ""));
+    });
+  });
+}
+
+function getLocalVersion() {
+  return require("./package.json").version;
+}
+
+async function updateToLatestVersion() {
+  const cp = exec("npm install -g gcook@latest --registry=http://registry.npmjs.org");
+  cp.stdout.on("end", () => {
+    process.send("restart");
+  });
+}
