de23a513419995cfd9e69c054918a905d38f173b
Denny <1160345031@qq.com>
2020-11-26 16:46:20 +0800

    文件名修改

--- start.js
+++ start.js
@@ -1,107 +1,36 @@
 #!/usr/bin/env node
-const { exec } = require("child_process");
-const path = require("path");
-const { connect } = require("socket.io-client");
+const { program } = require("commander");
+const { resolve,normalize } = require("path");
+const res = (command) => resolve(__dirname, "commands", command);
 
-const command = process.argv.slice(2);
+// 定义当前版本
+program.version(
+  require("./package.json").version,
+  "-v, --vers",
+  "output the current version"
+);
 
-(async function cook() {
-  const hasChanges = await hasCodeChanges();
-  if (hasChanges) {
-    console.log("Please git commit your changes!");
-    return;
-  }
+program.usage("<command>");
 
-  const latest = await getLatestVersion();
-  const local = getLocalVersion();
-
-  if (latest !== local) {
-    console.log(`Please install latest version: npm install -g @choicefe/gcook@${latest}`);
-    return;
-  }
-
-  const outs = [];
-
-  const p = path.resolve(__dirname, "node_modules", "@choicefe/gagli", "bin", "cook");
-
-  const cp = exec(`cd ${process.cwd()} && node ${p} ${command}`);
-
-  cp.stdout.on("data", (data) => {
-    outs.push(data);
-    console.log(data);
+program
+  .command("batch")
+  .requiredOption("-p, --path <string>", "Config.js Path")
+  .option("-a, --all", "All message")
+  .description("npm包批量发布。。。")
+  .action((data) => {
+    require(res("batchPubilsh.js"))(data);
   });
-
-  cp.stdout.on("end", async () => {
-    if (outs.length === 0) {
-      console.log("Try again!");
-      return;
-    }
-
-    if (outs[outs.length - 1].indexOf("发布成功") === -1) {
-      return;
-    }
-    const info = await getGitInfo();
-    const branch = await getBranch();
-    const [author, commit] = info.split("#");
-
-    const out = outs[outs.length - 2];
-    const [name, version] = out.replace("+ ", "").split(/@(?=\d)/);
-
-    const io = connect("http://office.choicesaas.cn/choicefe");
-    io.emit("update", `${author}#${name}#${version}#${branch}#${commit}`);
-
-    setTimeout(() => {
-      io.close();
-    }, 1000);
+program
+  .command("beta")
+  .description("beta 版本发布。。。")
+  .action((data) => {
+    require(res("gcook.js"))("beta");
   });
-})();
-
-async function getGitInfo() {
-  return new Promise((resolve) => {
-    const cp = exec(`git log --pretty=format:"%an#%s" -1`);
-    cp.stdout.on("data", (data) => {
-      resolve(data);
-    });
+program
+  .command("publish")
+  .description("publish 版本发布。。。")
+  .action((data) => {
+    require(res("gcook.js"))("publish");
   });
-}
-
-async function getBranch() {
-  return new Promise((resolve) => {
-    const cp = exec("git branch");
-    cp.stdout.on("data", (data) => {
-      const branch = data
-        .split("\n")
-        .filter((b) => b.startsWith("*"))
-        .join("")
-        .replace("* ", "");
-      resolve(branch);
-    });
-  });
-}
-
-async function getLatestVersion() {
-  return new Promise((resolve) => {
-    const cp = exec("npm view @choicefe/gcook version");
-    cp.stdout.on("data", (version) => {
-      resolve(version.replace(/(\r|\n)/g, ""));
-    });
-  });
-}
-
-function getLocalVersion() {
-  return require("./package.json").version;
-}
-
-async function hasCodeChanges() {
-  return new Promise((resolve) => {
-    exec("git status", (error, stdout) => {
-      if (!error) {
-        if (stdout.includes("Changes not staged for commit") || stdout.includes("Changes to be committed")) {
-          resolve(true);
-        }
-        resolve(false);
-      }
-      resolve(true);
-    });
-  });
-}
+  // console.log(process.argv)
+program.parse(process.argv);
